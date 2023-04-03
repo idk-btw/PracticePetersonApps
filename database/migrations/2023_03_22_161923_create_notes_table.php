9305e8f8b84cd852d3a6caa87b7f52f3a5fd5271
Max Gonak <maksim.gonak@gmail.com>
2023-04-03 13:31:03 +0300

    Add Stage and Type columns to notes

--- database/migrations/2023_03_22_161923_create_notes_table.php
+++ database/migrations/2023_03_22_161923_create_notes_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
             $table->string('title')->unique();
-            $table->text('description');
-            $table->double('hours_spend');
-            $table->integer('stage');
+            $table->text('description')->nullable();
+            $table->double('hours_spend')->nullable();
+            $table->integer('stage')->default(0);
             $table->string('comments')->nullable();
             $table->string('type');
             $table->timestamps();
